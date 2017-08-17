6b210f04240250f8155826e1b25b6c85d884ed15
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-08-17 15:11:29 -0400

    build: Fix the build
    The bot that produced the last commit has gone rogue. Extra safety
    will be added so it doesn't happen again.

--- src/pixmapmanipulatordefault.h
+++ src/pixmapmanipulatordefault.h
@@ -29,8 +29,8 @@ public:
     QVariant   callPhoto(const ContactMethod* n, const QSize& size, bool displayPresence = true) override;
     QVariant   numberCategoryIcon(const QVariant& p, const QSize& size, bool displayPresence = false, bool isPresent = false) override;
     QVariant   securityIssueIcon(const QModelIndex& index) override;
-    QByteArray toByteArray(const QVariant& pxm, const QString& type = QStringLiteral(QStringLiteral("PNG"))) override;
-    QVariant   personPhoto(const QByteArray& data, const QString& type = QStringLiteral(QStringLiteral("PNG"))) override;
+    QByteArray toByteArray(const QVariant& pxm, const QString& type = QStringLiteral("PNG")) override;
+    QVariant   personPhoto(const QByteArray& data, const QString& type = QStringLiteral("PNG")) override;
     QVariant   collectionIcon(const CollectionInterface* interface, PixmapManipulatorI::CollectionIconHint hint = PixmapManipulatorI::CollectionIconHint::NONE) const override;
     QVariant   securityLevelIcon(const SecurityEvaluationModel::SecurityLevel level) const override;
     QVariant   historySortingCategoryIcon(const CategorizedHistoryModel::SortedProxy::Categories cat) const override;
