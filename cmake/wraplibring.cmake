f144fa7d155260ac4c185cf946b228119069bdda
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-10-20 16:12:37 +0200

    cmake: Remove libqtwrapper and add the files directly
    There was issues with the exported target. Some people argue,
    including me that it's a dumb design bug in CMake, but the
    workaround is either to stop using sub-libraries or to do
    ugly, terrible things.

---
+++ cmake/wraplibring.cmake
@@ -0,0 +1,33 @@
+
+MESSAGE(STATUS "Compiling with qtwrapper")
+
+ADD_DEFINITIONS(-DENABLE_LIBWRAP=true)
+
+IF (${MUTE_DRING} MATCHES "ON")
+   #It makes clients debugging easier
+   MESSAGE(STATUS "Dring logs are disabled")
+   ADD_DEFINITIONS(-DMUTE_DRING=true)
+ENDIF()
+
+SET(CMAKE_AUTOMOC TRUE)
+
+SET(libringclient_LIB_SRCS
+   ${libringclient_LIB_SRCS}
+   src/qtwrapper/instancemanager.cpp
+   src/qtwrapper/callmanager_wrap.h
+   src/qtwrapper/configurationmanager_wrap.h
+   src/qtwrapper/instancemanager_wrap.h
+   src/qtwrapper/presencemanager_wrap.h
+   src/qtwrapper/videomanager_wrap.h
+)
+
+QT5_WRAP_CPP(LIB_WRAPPER_HEADER_MOC ${libqtwrapper_LIB_SRCS})
+
+IF(NOT (${ENABLE_VIDEO} MATCHES "false"))
+   MESSAGE(STATUS "VIDEO enabled")
+   ADD_DEFINITIONS(-DENABLE_VIDEO=true)
+   SET(ENABLE_VIDEO 1 CACHE BOOLEAN "Enable video")
+ENDIF(NOT (${ENABLE_VIDEO} MATCHES "false"))
+
+INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/src/qtwrapper)
+INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/src/dbus)
