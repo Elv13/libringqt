a76782d934269a74aa85a3065e095e432ba89dac
Emmanuel Lepage Vallee <elv1313@gmail.com>
2017-10-20 18:51:19 +0200

    cmake: Cleanup
    Remove some dead code, add comments and refactor to reduce the
    number of "if".

--- cmake/wraplibring.cmake
+++ cmake/wraplibring.cmake
@@ -3,6 +3,15 @@ MESSAGE(STATUS "Compiling with qtwrapper")
 
 ADD_DEFINITIONS(-DENABLE_LIBWRAP=true)
 
+# OS X
+IF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+   SET(CMAKE_MACOSX_RPATH ON)
+   SET(CMAKE_SKIP_BUILD_RPATH FALSE)
+   SET(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE)
+   SET(CMAKE_INSTALL_RPATH "${CMAKE_CURRENT_SOURCE_DIR}")
+   SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+ENDIF(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+
 IF (${MUTE_DRING} MATCHES "ON")
    #It makes clients debugging easier
    MESSAGE(STATUS "Dring logs are disabled")
@@ -33,6 +42,12 @@ ADD_DEFINITIONS(-fPIC)
 IF(${VERBOSE_IPC} MATCHES "ON")
    MESSAGE(STATUS "Adding more debug output")
    ADD_DEFINITIONS(-DVERBOSE_IPC=true)
+ENDIF()
+
+# Allow building with undefined symbols when only the daemon headers are provided
+# It speeds up our CI builds
+IF(${ring_BIN} MATCHES "ring_BIN-NOTFOUND" AND "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+    SET_TARGET_PROPERTIES( ringclient PROPERTIES LINK_FLAGS "-undefined dynamic_lookup" )
 ENDIF()
 
 INCLUDE_DIRECTORIES(${CMAKE_CURRENT_SOURCE_DIR}/src/qtwrapper)
