5055378ea007836856fe1c55e8ae2dc0ec664dda
Andreas Traczyk <andreas.traczyk@savoirfairelinux.com>
2018-08-14 11:28:35 -0400

    debug: connect to debug signal and forward to client
    Change-Id: I0601aa7b323e2213000571481061d0eb707580de

--- src/qtwrapper/configurationmanager_wrap.h
+++ src/qtwrapper/configurationmanager_wrap.h
@@ -53,6 +53,7 @@ public:
         using DRing::ConfigurationSignal;
         using DRing::AudioSignal;
         using DRing::DataTransferSignal;
+        using DRing::DebugSignal;
 
         setObjectName("ConfigurationManagerInterface");
         confHandlers = {
@@ -152,6 +153,10 @@ public:
             exportable_callback<ConfigurationSignal::ContactRemoved>(
                 [this] (const std::string& account_id, const std::string& uri, const bool& banned) {
                     Q_EMIT this->contactRemoved(QString(account_id.c_str()), QString(uri.c_str()), banned);
+                }),
+            exportable_callback<DebugSignal::MessageSend>(
+                [this](const std::string& message) {
+                    Q_EMIT this->debugMessageReceived(message);
                 }),
         };
 
@@ -686,7 +691,7 @@ Q_SIGNALS: // SIGNALS
     void migrationEnded(const QString &accountID, const QString &result);
     void contactAdded(const QString &accountID, const QString &uri, bool banned);
     void contactRemoved(const QString &accountID, const QString &uri, bool banned);
-    void dataTransferEvent(qulonglong transfer_id, uint code);
+    void debugMessageReceived(const std::string& message);
 };
 
 namespace org { namespace ring { namespace Ring {
