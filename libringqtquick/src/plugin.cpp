7c45b7409d16868d8e11b976f0c635559bca1b0b
Emmanuel Lepage Vallee <elv1313@gmail.com>
2019-01-11 23:34:01 -0500

    qtquick: Register more types.
    This is a step toward having pure QML (no C++ main) clients for
    LibRingQt.

--- libringqtquick/src/plugin.cpp
+++ libringqtquick/src/plugin.cpp
@@ -26,9 +26,92 @@
 #include "accountfields.h"
 #include "accountbuilder.h"
 
+// Data objects
+#include <account.h>
+#include <call.h>
+#include <person.h>
+#include <individual.h>
+#include <contactmethod.h>
+#include <event.h>
+#include <media/recording.h>
+#include <media/textrecording.h>
+#include <media/avrecording.h>
+#include <media/availabilitytracker.h>
+#include <video/sourcemodel.h>
+#include <video/renderer.h>
+#include <troubleshoot/dispatcher.h>
+#include <troubleshoot/base.h>
+
+// Models
+#include <useractionmodel.h>
+#include <individualtimelinemodel.h>
+#include <pendingcontactrequestmodel.h>
+#include <securityevaluationmodel.h>
+#include <ringdevicemodel.h>
+#include <codecmodel.h>
+#include <credentialmodel.h>
+#include <protocolmodel.h>
+#include <numbercompletionmodel.h>
+
+#define QML_TYPE(name) qmlRegisterUncreatableType<name>(uri, 1,0, #name, #name "cannot be instantiated");
+#define QML_TYPE_MED(name) qmlRegisterUncreatableType<name>(meduri, 1,0, #name, #name "cannot be instantiated");
+#define QML_TYPE_MOD(name) qmlRegisterUncreatableType<name>(moduri, 1,0, #name, #name "cannot be instantiated");
+#define QML_TYPE_VID(name) qmlRegisterUncreatableType<name>(viduri, 1,0, #name, #name "cannot be instantiated");
+#define QML_TYPE_VIM(name) qmlRegisterUncreatableType<name>(vimuri, 1,0, #name, #name "cannot be instantiated");
+#define QML_TYPE_TRO(name) qmlRegisterUncreatableType<name>(trouri, 1,0, #name, #name "cannot be instantiated");
+#define QML_NS(name) qmlRegisterUncreatableMetaObject( name :: staticMetaObject, #name, 1, 0, #name, "Namespaces cannot be instantiated" );
+#define QML_CRTYPE(name) qmlRegisterType<name>(uri, 1,0, #name);
+
 void RingQtQuick::registerTypes(const char *uri)
 {
     Q_ASSERT(uri == QLatin1String("net.lvindustries.ringqtquick"));
+    static QByteArray moduri = QByteArray(uri)+".models";
+    static QByteArray meduri = QByteArray(uri)+".media";
+    static QByteArray viduri = QByteArray(uri)+".video";
+    static QByteArray vimuri = QByteArray(uri)+".video.models";
+    static QByteArray trouri = QByteArray(uri)+".troubleshoot";
+
+    // Uncreatable types
+    QML_TYPE( Account       )
+    QML_TYPE( const Account )
+    QML_TYPE( Call          )
+    QML_TYPE( Person        )
+    QML_TYPE( Individual    )
+    QML_TYPE( ContactMethod )
+    QML_TYPE( Event         )
+
+    // Uncreatable models
+    QML_TYPE_MOD( UserActionModel            )
+    QML_TYPE_MOD( IndividualTimelineModel    )
+    QML_TYPE_MOD( PendingContactRequestModel )
+    QML_TYPE_MOD( SecurityEvaluationModel    )
+    QML_TYPE_MOD( RingDeviceModel            )
+    QML_TYPE_MOD( CodecModel                 )
+    QML_TYPE_MOD( CredentialModel            )
+    QML_TYPE_MOD( ProtocolModel              )
+
+    qmlRegisterType<NumberCompletionModel>(moduri, 1,0, "NumberCompletionModel");
+
+    // Media subsystem
+    { using namespace Media;
+        QML_TYPE_MED( Recording     );
+        QML_TYPE_MED( TextRecording );
+        QML_TYPE_MED( AVRecording   );
+
+        qmlRegisterType<AvailabilityTracker>(meduri, 1,0, "AvailabilityTracker");
+    }
+
+    // Video subsystem
+    { using namespace Video;
+        QML_TYPE_VIM( SourceModel );
+        QML_TYPE_VID( Renderer    );
+    }
+
+    // Live troubleshooting subsystem
+    { using namespace Troubleshoot;
+        QML_TYPE_TRO( Base )
+        qmlRegisterType<Dispatcher>(trouri, 1,0, "Dispatcher");
+    }
 
     qmlRegisterType<AccountFields> (uri, 1, 0, "AccountFields" );
     qmlRegisterType<AccountBuilder>(uri, 1, 0, "AccountBuilder");
