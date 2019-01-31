f9727b89779a9124576e445f84b4c61d77c4eeb6
Emmanuel Lepage Vallee <elv1313@gmail.com>
2019-01-31 14:11:30 -0500

    picoCMS: 11th attemps to prevent QtQuick GC from reaping objects.

--- src/picocms/itembase.cpp
+++ src/picocms/itembase.cpp
@@ -19,28 +19,98 @@
 
 #include <QtCore/QTimer>
 
+ /**
+  * All object need AT ALL TIME a parent or QtQuick GC will reap them.
+  *
+  * The parent has to be in the same thread or QtQuick GC will still reap them
+  * because reparenting failed.
+  *
+  * Make sure they are force-reparented when the thread is destroyed or
+  * qDeleteAll will reap them.
+  *
+  * What else have failed:
+  *
+  * * Adding a dumb QTimer::singleShot: Qt will reap the objects if the thread
+  *   hangs on some slot I/O
+  *
+  * * Not setting a parent: GC will reap it
+  *
+  * * Adding a QList and batching the transfer: GC will reap the objects if the
+  *   queue has not been dealt with yet
+  *
+  * * Having a global object per thread and assigning it as parent while waiting
+  *   to transfer them: The transfer will still have a window of opportunity
+  *   when the setParent has to be reset before calling moveToThread and the
+  *   GC will reap them.
+  *
+  * The idea here is that the `setParent` is always called in the same thread
+  * and wont ever get corrupted. Reparenting can happen later on in a queued
+  * slot.
+  *
+  */
+class TemporaryParentHolder
+{
+public:
+    QHash<QThread*, QObject*> m_hHolder;
+    void hold(QObject* o, QObject* p);
+    static TemporaryParentHolder* instance();
+};
+
+TemporaryParentHolder* TemporaryParentHolder::instance()
+{
+    static TemporaryParentHolder h;
+    return &h;
+}
+
+void TemporaryParentHolder::hold(QObject* o, QObject* p)
+{
+    Q_ASSERT(p && o);
+    static QMutex m;
+    QMutexLocker l(&m);
+
+    if (!m_hHolder.contains(p->thread())) {
+        auto n = new QObject();
+        n->moveToThread(p->thread());
+        n->setParent(p);
+        m_hHolder[p->thread()] = p;
+    }
+
+    Q_ASSERT(m_hHolder[p->thread()]->thread() == p->thread());
+    o->moveToThread(QCoreApplication::instance()->thread());
+    o->setParent(m_hHolder[p->thread()]);
+
+    auto t = new QTimer();
+    t->setInterval(0);
+
+    // Call setParent from the correct thread
+    QObject::connect(t, &QTimer::timeout, p, [t, o, p]() {
+        o->setParent(p);
+        t->stop();
+        t->deleteLater();
+    }, Qt::QueuedConnection);
+}
+
 ItemBase::ItemBase(QObject* parent) :QObject(nullptr), d_ptr(new ItemBasePrivate())
 {
-   // Otherwise QML will claim and destroy them
-   //Q_ASSERT(parent);
+    // If the parent isn't in the main thread, it will eventually crash, it isn't
+    // safe and isn't supported.
+    Q_ASSERT((!parent) || parent->thread() == QCoreApplication::instance()->thread());
 
-   // Because after moveToThread, the code is still running in the old thread,
-   // setting the parent may be done in multiple threads in parallel on the
-   // same parent. In that case the parent object children list is potentially
-   // being resized in *another thread context*. This avoids causing such issue.
-   if (QCoreApplication::instance()->thread() != thread()) {
-      // Set a temporary parent to be certain that if a thread event loop
-      // iteration takes a long time, there is no window of opportunity for
-      // QtQuick to claim and free the object.
-      setParent(QCoreApplication::instance());
+    // Otherwise QML will claim and destroy them
+    //Q_ASSERT(parent);
 
-      QObject::moveToThread(QCoreApplication::instance()->thread());
-      QTimer::singleShot(0, [this, parent]() {
-         QObject::setParent(parent);
-      });
-   }
-   else
-       QObject::setParent(parent);
+    // Because after moveToThread, the code is still running in the old thread,
+    // setting the parent may be done in multiple threads in parallel on the
+    // same parent. In that case the parent object children list is potentially
+    // being resized in *another thread context*. This avoids causing such issue.
+    if (parent->thread() != thread()) {
+        // Set a temporary parent to be certain that if a thread event loop
+        // iteration takes a long time, there is no window of opportunity for
+        // QtQuick to claim and free the object.
+        TemporaryParentHolder::instance()->hold(this, parent);
+    }
+    else
+        QObject::setParent(parent);
 }
 
 ItemBase::~ItemBase()
